a9eee2b04eb6c0c4c61e41d715044eb0c01b9d2b
reshmajagan <reshmajagan@gmail.com>
2023-10-23 17:18:16 +0200

    Created a separate folder for classes and added documentation

---
+++ classes/Box.js
@@ -0,0 +1,45 @@
+class Box {
+  /** 
+   * x and y:  x & y coordinates denoting the center point of the Box object
+   * width: width of box
+   * height: height of box
+   * color: color of the box
+   */
+  constructor(x, y, width, height, color) {
+    
+    var options = {
+      /**options store the various properties that Box objects should possess */
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options); /**This created a rectangle shaped body */
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    
+    /**Adding the newly created body to the "world" created in sketch.js, using "World.add()" */
+    World.add(world, this.body);
+  }
+
+  display(){
+    /**
+     * In the constructor above, a rectangle shaped body was created.
+     * But the body by itself is not visible on screen, but it can affect other objects in its surroundings.
+     * For eg: If one body without a display was made to fall on another visible body, we will see the 2nd body moving away due to the impact.
+     * So we have to draw a simple rectangle with the same size as the body and move it whenever the body moves.
+     * This rectangle will act as the display for the body created above.
+     * That is being done in display()
+     */
+
+    var pos =this.body.position;
+  
+    push();
+    translate(pos.x, pos.y);
+    rotate(this.body.angle);
+    rectMode(CENTER);
+    fill(this.color);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
---
+++ classes/Ground.js
@@ -0,0 +1,29 @@
+class Ground {
+    constructor(x,y,width,height) {
+      /** x and y:  x & y coordinates denoting the center point of the Ground object */
+      var options = {
+          /**this makes the ground static */
+          isStatic: true            
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options); /** this creates a rectangle shaped body */
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body); /**this adds the new body to the "world" created before */
+    }
+
+    display(){
+    /**
+     * In the constructor above, a rectangle shaped body was created.
+     * But the body by itself is not visible on screen, but it can affect other objects in its surroundings.
+     * For eg: if the ground body without a display was made to stay in the middle of the canvas, and a box body with a display falls from the top of the screen, the box will fall on top of the ground and stop moving
+     * So we have to draw a simple rectangle with the same size as the body and move it whenever the body moves. 
+     * This rectangle will act as the display for the body created above.
+     * That is being done in display()
+     */
+
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill(255);
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
